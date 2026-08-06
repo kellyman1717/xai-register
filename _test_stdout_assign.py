54975204a0304fd1d0c96db4fe7d56778f66cd3e
kellyman1717 <akuntumbal9419@gmail.com>
2026-08-06 16:37:38 +0700

    Remove debug and capture utilities

--- _test_stdout_assign.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-class X:
-    pass
-
-print("before:", type(sys.stdout).__name__)
-sys.stdout = X()
-print("after:", type(sys.stdout).__name__)
-print("is X:", isinstance(sys.stdout, X))
